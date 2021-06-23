30a96cada4b76695b723439677f570e7e983ddeb
Bullet Magnet <bulletmagnet.123@gmail.com>
2021-06-22 17:44:43 -0700

    lastexercise

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,43 +1,21 @@
-import React, { Component } from "react";
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardText,
-  CardBody,
-  CardTitle,
-} from "reactstrap";
+import React from "react";
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-class CampsiteInfo extends React.Component {
-  renderCampsite(campsite) {
+
+function RenderCampsite({campsite}) {
     return (
       <div className="col-md5 m-1">
         <Card>
           <CardImg top src={campsite.image} alt={campsite.name} />
           <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
+                <CardText>{campsite.description}</CardText>
           </CardBody>
         </Card>
       </div>
-    );
-  }
-  render() {
-    console.log(this.props);
-    if (this.props.campsite){
-      return (
-      <div className="container"> 
-      <div className="row">
-        {this.renderCampsite(this.props.campsite)}
-        {this.renderComments(this.props.campsite.comments)}
-      </div>
-      </div>
-      );
-    }
-    return <div/>
-  }
-  renderComments(comments){
-    console.log(comments)
+  );
+}  
+function RenderComments({comments}){
     if (comments){
       return (
         <div className="col-md-5 m-1">
@@ -51,10 +29,34 @@ class CampsiteInfo extends React.Component {
               )
             })}
         </div>
-      )
+      );
     }
+    return <div />
+}
+function CampsiteInfo (props) {
+    if (props.campsite){
+      return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>{props.campsite.name}</h2>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
+                <RenderCampsite campsite={props.campsite} />
+                <RenderComments comments={props.comments} />
+            </div>
+        </div>
+    );
   }
 }
+
+
 
 
 export default CampsiteInfo;
