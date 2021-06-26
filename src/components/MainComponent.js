2a9b7435f5907277280ab3a7f0af561092ffa24d
Nathaniel Phelps <wildnate@gmail.com>
2021-06-26 11:35:12 -0700

    thirdworkshop

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { CAMPSITES } from "../shared/campsites";
 import { COMMENTS } from "../shared/comments";
 import { PARTNERS } from "../shared/partners";
 import { PROMOTIONS } from "../shared/promotions";
+import About from "./AboutComponent";
 
 class Main extends Component {
   constructor(props) {
@@ -19,6 +20,7 @@ class Main extends Component {
       comments: COMMENTS,
       partners: PARTNERS,
       promotions: PROMOTIONS,
+
     };
   }
 
@@ -62,9 +64,9 @@ class Main extends Component {
           <Route
             exact
             path="/directory"
-            render={() => <Directory campsites={this.state.campsites} />}
-          />
+            render={() => <Directory campsites={this.state.campsites} />} />
           <Route path="/directory/:campsiteId" component={CampsiteWithId} />
+          <Route exact path="/aboutus" render={() => <About partners={this.state.partners}/>}/>
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
