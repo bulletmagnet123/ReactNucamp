0d04d819b234bd7740ef16e1974334e9ee9dfac8
Bulletmagnet <bulletmagnet.123@gmail.com>
2021-07-03 12:52:26 -0700

    week4 assignment done

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   Card,
   CardImg,
@@ -6,11 +6,19 @@ import {
   CardBody,
   Breadcrumb,
   BreadcrumbItem,
+  Button,
+  Modal, ModalHeader, ModalBody,
+  Form, FormGroup, Input, Label, Col
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
 
 
-function RenderCampsite({campsite}) {
+function RenderCampsite({ campsite }) {
   return (
     <div class="col-md-5 m-1">
       <Card>
@@ -22,8 +30,88 @@ function RenderCampsite({campsite}) {
     </div>
   );
 }
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
 
-function RenderComments({comments}) {
+    this.state = {
+      isNavOpen: false,
+      isModalOpen: false
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
+  handleSubmit(values) {
+    console.log('Current state is: ' + JSON.stringify(values));
+    alert('Current state is: ' + JSON.stringify(values));
+  }
+
+  render() {
+    return (
+      <div>
+        <Button outline className="fa-lg fa fa-pencil" onClick={this.toggleModal}>Submit Comment</Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={values => this.handleSubmit(values)}>
+              <label htmlFor="rating">Ratings:</label>
+              <Control.select model=".rating" id="rating">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </Control.select>
+              <Col>
+              <Label htmlFor="author" md={2}>
+                Your Name
+                </Label>
+                <Control.text model=".author" id=".author" name="author"
+                  placeholder="Your Name"
+                  className="form-control"
+                  validators={{
+                    required,
+                    minLength: minLength(2),
+                    maxLength: maxLength(15)
+                  }}
+                />
+                <Errors
+                    className="text-danger"
+                    model=".author"
+                    show="touched"
+                    Component="div"
+                    messages={{
+                      required: 'Required',
+                      minLength: 'Must be at least 2 characters',
+                      maxLength: 'Must be 15 characters or less'
+                    }}
+                  />
+                </Col>
+                <Label htmlFor="author" md={2}>
+                Text
+                </Label>
+                <Control.textarea model=".text" id="text" name="text"
+                  placeholder="Text" row={6}
+                  className="form-control"
+                />
+                <Button type="submit" color="primary">
+                    Send Feedback
+                  </Button>
+            </LocalForm >
+          </ModalBody>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+function RenderComments({ comments }) {
   console.log(comments);
   if (comments) {
     return (
@@ -45,6 +133,8 @@ function RenderComments({comments}) {
             </div>
           );
         })}
+        <CommentForm />
+
       </div>
     );
   }
